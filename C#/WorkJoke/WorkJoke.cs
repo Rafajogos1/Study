1dfe8371d5e597aeb684c3e5d19a93011f4fbcbf
Rafajogos1 <97739509+Rafajogos1@users.noreply.github.com>
2023-08-14 23:02:00 +0100

    Made a joke in C#

---
+++ C#/WorkJoke/WorkJoke.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class workJoke
+{
+    static void Main()
+    {
+        bool is_working = true;
+
+        if (is_working)
+            Console.WriteLine("Please do not disturb.");
+    }
+}
