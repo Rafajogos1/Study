9612336823f4e7109421829137a7c8c5f1d01349
Rafael <rafael.mpr.gameiro@gmail.com>
2024-10-22 18:48:34 +0100

    Creating a custom library. It currently only has one function.

---
+++ C/Custom Library/st_print_utils.c
@@ -0,0 +1,8 @@
+#include "stratocaster.h"
+
+/* This function prints a "new line" character. */
+/* Its purpose is just to make it quicker to write. */
+void	print_nl(void)
+{
+	printf("\n");
+}
---
+++ C/Custom Library/stratocaster.h
@@ -0,0 +1,10 @@
+/* Stratocasters are cool */
+
+#ifndef	STRATOCASTER_H
+# define	STRATOCASTER_H
+# include <stdlib.h>
+# include <stdio.h>
+
+void	print_nl(void);
+
+#endif
